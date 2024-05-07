c7321263fd0d014c024bc4ee19857becf34caea8
nikww <nikw2008@mail.ru>
2024-05-07 19:09:18 +0300

    Добавьте файлы проекта.

---
+++ AvaloniaApplication4/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AvaloniaApplication4.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ AvaloniaApplication4/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaApplication4.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
